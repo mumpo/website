d42c19dc8620c5011635f023cdf8c0ffee18a791
Sergi Férriz <sergiferriz@gmail.com>
2020-02-06 16:41:03 +0100

    Updates

---
+++ precache-manifest.063b96ced7421390217e24b4dde5303b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6883f24e9a03764359380981218cd7ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9749c9bc4a17ff06b08a",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5a03d42c642ddff72f59",
+    "url": "/static/js/2.781591f9.chunk.js"
+  },
+  {
+    "revision": "9749c9bc4a17ff06b08a",
+    "url": "/static/js/main.2ead7840.chunk.js"
+  },
+  {
+    "revision": "1fc694cc674354c1815e",
+    "url": "/static/js/runtime-main.7f0f7dfc.js"
+  },
+  {
+    "revision": "12427a45f69bdaed95fccf41867ee735",
+    "url": "/static/media/background-dark.12427a45.jpg"
+  },
+  {
+    "revision": "df0212fe1f8f97df126640d138abc244",
+    "url": "/static/media/background-light.df0212fe.jpg"
+  },
+  {
+    "revision": "f2954e87c9afb7db6c6836c9dfd400da",
+    "url": "/static/media/me.f2954e87.jpg"
+  }
+]);
